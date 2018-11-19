acca35df8fa9cd4cf99b7a7fea36e52d0fa2b4c7
github username <github email address>
2018-11-18 22:09:39 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pidgeons = {}
+  data.each do |attribute, values|
+    values.each do |value, pidgeons|
+      pidgeons.each do |pidgeon|
+        if new_pidgeons.keys.include?(pidgeon)
+          new_pidgeons[pidgeon][attribute] << value.to_s
+        else
+          new_pidgeons[pidgeon] = {:color => [], :gender => [], :lives => []}
+          new_pidgeons[pidgeon][attribute] << value.to_s
+        end
+      end
+    end
+  end
+  puts new_pidgeons
+  new_pidgeons
 end
